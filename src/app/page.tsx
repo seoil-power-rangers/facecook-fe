9f779a16370916670f05184664f54fcdbb3d1134
catlover <jimini1183@naver.com>
2026-08-28 06:04:13 +0900

    메인: 참가자 목록 화면 추가
    오늘의 콕/통계 요약, 지금 활동 중인 사람 가로 스크롤, 학과·MBTI·취미
    필터 칩, 참가자 리스트(콕 버튼으로 확인 시트, 카드 클릭으로 프로필
    상세 이동)를 구현. 루트 라우트(/)에 연결.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
+import { MainScreen } from "@ui/메인/MainScreen";
+
 export default function Home() {
-  return (
-    <main>
-      <h1>face 콕</h1>
-      <p>작업은 ui / function / db 폴더에서 하면 됩니다. README.md를 보세요.</p>
-    </main>
-  );
+  return <MainScreen />;
 }
